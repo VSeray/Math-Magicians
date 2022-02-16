32e6fca47e4a78e685f26783383fce16894e22af
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-02-16 10:04:30 +0000

    Add test to calculate function

---
+++ src/tests/unit_testing/calculate.test.js
@@ -0,0 +1,19 @@
+import calculate from '../../logic/calculate';
+
+describe('testing calculate function', () => {
+  const obj = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+  test('adding a number when the next is null', () => {
+    const result = calculate(obj, '5');
+    expect(result.next).toBe('5');
+  });
+
+  test('adding a number when the next is true', () => {
+    let result = calculate(obj, '5');
+    result = calculate(result, '6');
+    expect(result.next).toBe('56');
+  });
+});
